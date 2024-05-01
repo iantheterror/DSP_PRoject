1210807a9897869f7e8ba60cfa374c65ad8a3eca
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2024-05-01 12:52:42 -0400

    edit

--- Code/deletexml.py
+++ Code/deletexml.py
@@ -1,15 +1,15 @@
 import os
 
 directory = 'resized_images/'
-
+N = 2
 files = os.listdir(directory)
 
 for filename in files:
-    if filename.startswith('resized') and filename.endswith('.xml.txt'):
+    if filename.startswith(f'resized{N}') and filename.endswith('.xml.txt'):
         try:
             n = int(filename.split('_')[1].split('.')[0])
             # Construct the new filename with the .xml extension removed
-            new_filename = f"resized_{n}.txt"
+            new_filename = f"resized{N}_{n}.txt"
             # Rename the file
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
             print(f"Renamed {filename} to {new_filename}")
