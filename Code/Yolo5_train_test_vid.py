621b0891fc98b9507bf9244e2a6be13e5fd26d95
IvanTheTerrier <112969078+iantheterror@users.noreply.github.com>
2024-05-03 10:35:06 -0400

    push

--- Code/Yolo5_train_test_vid.py
+++ Code/Yolo5_train_test_vid.py
@@ -1,18 +1,23 @@
 
 import torch
 import cv2
+from playsound import playsound
 batch = "exp7"
 # exp4 is 10 epoch, exp5 is 50 epoch,  exp6 is the special dataset
 # path = path = 'F:\\Git\\DSP\\DSP_PRoject\\Code\\yolov5-master\\'  #Your File PAth HEre
 path = 'F:\\Git\\DSP\\DSP_PRoject\\Code\\yolov5-master\\'
 pathapp = path + f'\\runs\\train\\{batch}\\weights\\last.pt'
-
-
+trig_class = 2 # 0: Ian 1: Tara 2: Ruby 3: Stop
+trig_thresh = 80
+run1 = False
+activate = False
+class_ = 5
+trig_Count = 0
 #model = torch.hub.load('C:\\Users\\luisa\\Dropbox\\UVMstuff\\AIR lab\\Yolo8_Training\\yolov5', 'last', source = 'local')
 model = torch.hub.load(path, 'custom', path=pathapp, force_reload=True,source='local') 
 
 cap = cv2.VideoCapture(0) 
-
+img2 = cv2.imread('RR.jpg')
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -28,13 +33,14 @@ while True:
         x1, y1 = int(df['xmin'][ind]), int(df['ymin'][ind])
         x2, y2 = int(df['xmax'][ind]), int(df['ymax'][ind])
         label = df['name'][ind]
+        class_ = df['class'][ind]
         confidence = int(df['confidence'][ind]*100)
         conf = f'Confidence: {confidence}%'
         if confidence < 50 :
             
             cv2.rectangle(img, (x1, y1), (x2,y2), (255, 0, 0), 2)
             cv2.putText(img, "Low Confidence", (x1, y1-5), cv2.FONT_HERSHEY_PLAIN, 2, (0,0 , 255), 2)
-            cv2.putText(img, conf, (x1+100, y1-5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv2.putText(img, conf, (x2, y1-5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         elif confidence >= 50 :
             if confidence >= 70:
                 cv2.rectangle(img, (x1, y1), (x2,y2), (255, 255, 0), 2)
@@ -44,9 +50,27 @@ while True:
                 cv2.rectangle(img, (x1, y1), (x2,y2), (255, 255, 0), 2)
                 cv2.putText(img, label, (x1, y1-5), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 2)
                 cv2.putText(img, conf, (x1+100, y1-5), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 2)
+    
+    
+
+    
+    if class_ == trig_class:
+        if run1 == False:
+            if confidence >= trig_thresh:
+                trig_Count +=1
+                if trig_Count >= 10:
+                    activate = True 
+                    run1 = True
+                    img = img2
+        
     cv2.imshow('IMAGE', img)
     cv2.waitKey(10)
-        
+    if activate == True:
+        playsound('RR.mp3')
+        activate = False
+
+
+          
     
 '''
 
